54cc25ab6b67edd8b5ffcfb9a58cc6a4e562014a
soni <soni@live.com>
2022-12-08 14:08:46 -0300

    inicio do projeto

---
+++ src/main/java/br/com/demo/mongocache/MongocacheApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.mongocache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongocacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongocacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/demo/mongocache/MongocacheApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.mongocache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongocacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
