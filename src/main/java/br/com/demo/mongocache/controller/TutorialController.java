a5ead29457aff09c42523f2ef1b0d7a13493dcac
SS <sonival.santos@os2h-alp-mac177.local>
2022-12-12 17:43:03 -0300

    Resolvido problema do valid  spring

--- src/main/java/br/com/demo/mongocache/controller/TutorialController.java
+++ src/main/java/br/com/demo/mongocache/controller/TutorialController.java
@@ -5,6 +5,7 @@ import br.com.demo.mongocache.repository.TutorialRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
@@ -53,14 +54,11 @@ public class TutorialController {
     
   } */
 
-  @PostMapping("/tutorials")
-  public ResponseEntity<Tutorial> createTutorial(@Valid @RequestBody Tutorial tutorial) {
-    try {
-      Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
-      return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  @PostMapping(value="/tutorials",  consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Tutorial> createTutorial(@RequestBody @Valid Tutorial tutorial) {
+      //Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+      return new ResponseEntity<>(new Tutorial(), HttpStatus.CREATED);
+   
   }
 
   /* @PutMapping("/tutorials/{id}")
--- src/main/java/br/com/demo/mongocache/model/Tutorial.java
+++ src/main/java/br/com/demo/mongocache/model/Tutorial.java
@@ -1,68 +1,77 @@
 package br.com.demo.mongocache.model;
 
+import java.io.Serializable;
+
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "tutorials")
-public class Tutorial {
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Tutorial implements Serializable {
 
   @Id
   private String id;
-  @NotNull
-  @NotBlank
+  
+  @NotNull(message = "˜not null˜")
+  @NotEmpty(message = "no empty")
   private String title;
-  @NotNull
-  @NotBlank
+
+  @NotNull(message = "˜not null˜")
+  @NotEmpty(message = "no empty")
+  @NotBlank(message = "not black")
   private String description;
 
   @NotNull
-  @Pattern(regexp = "^true$|^false$", message = "allowed input: true or false")
   private boolean published;
 
-  public Tutorial() {
+  // public Tutorial() {
+  //   this.description = null;
+  // }
 
-  }
+  // public Tutorial(String title, String description, boolean published) {
+  //   this.title = title;
+  //   this.description = description;
+  //   this.published = published;
+  // }
 
-  public Tutorial(String title, String description, boolean published) {
-    this.title = title;
-    this.description = description;
-    this.published = published;
-  }
+  // public String getId() {
+  //   return id;
+  // }
 
-  public String getId() {
-    return id;
-  }
+  // public String getTitle() {
+  //   return title;
+  // }
 
-  public String getTitle() {
-    return title;
-  }
+  // public void setTitle(String title) {
+  //   this.title = title;
+  // }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+  // public String getDescription() {
+  //   return description;
+  // }
 
-  public String getDescription() {
-    return description;
-  }
+  // public void setDescription(String description) {
+  //   this.description = description;
+  // }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+  // public boolean isPublished() {
+  //   return published;
+  // }
 
-  public boolean isPublished() {
-    return published;
-  }
+  // public void setPublished(boolean isPublished) {
+  //   this.published = isPublished;
+  // }
 
-  public void setPublished(boolean isPublished) {
-    this.published = isPublished;
-  }
-
-  @Override
-  public String toString() {
-    return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
-  }
+  // @Override
+  // public String toString() {
+  //   return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+  // }
 }
